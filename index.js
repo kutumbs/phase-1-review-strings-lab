a6683c62d178b4415003512dd602723745f14359
jonathan <jonathan.123kutumbakana@gmail.com>
2023-12-13 14:01:47 +0300

    done assignemt

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = 'Jonathan kutumbakana';
+const welcomeMessage = 'Welcome to Flatbook,${currentUser.slice(0,1)}!';
+//AssertionError: "expected 'Welcome to Flatbook, currentUser' to contain 'Grace Hopper'"
+const shortGreeting = 'Welcome, ';
+
